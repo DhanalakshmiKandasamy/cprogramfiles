c008f3600e8bfb13a205a0fe75c1e93fd3f8a64c
DhanalakshmiKandasamy <dhanalakshmibca2@gmail.com>
2017-06-22 19:16:18 +0530

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 void main()
 {
-int n,d,t,s=0;
+int n,d,s=0;
 printf("Enter the number");
 scanf("%d",&n);
 while(n>0)
 {
 	d=n%10;
 	s=s*10+d;
-	t=n/10;
+	n=n/10;
 }
 printf("Reverse number is: %d",s);
 }
