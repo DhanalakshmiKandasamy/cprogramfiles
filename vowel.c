4a7c84fd0b594eaee721093279eba8554d1da5ee
DhanalakshmiKandasamy <dhanalakshmibca2@gmail.com>
2017-06-22 23:08:56 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -4,7 +4,7 @@ int main()
 char c;
 printf("\nEnter the character");
 scanf("%c",&c);
-if((c=='a' || c=='e' c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U')
+if(c=='a' || c=='e'|| c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U')
 printf("\n%c is Vowel",c);
 else
 printf("\n%c is Consonant",c);
