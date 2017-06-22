cc1bae8b7d6acb59b2fc7fc3c5f3d0b73b8f0d77
DhanalakshmiKandasamy <dhanalakshmibca2@gmail.com>
2017-06-22 23:06:38 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+char c;
+printf("\nEnter the character");
+scanf("%c",&c);
+if((c=='a' || c=='e' c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c=='I' || c=='O' || c=='U')
+printf("\n%c is Vowel",c);
+else
+printf("\n%c is Consonant",c);
+return 0;
+}
