b49fa3e0d5d6e9047dfd660abede211f3473ad0a
DhanalakshmiKandasamy <dhanalakshmibca2@gmail.com>
2017-06-23 10:31:55 +0530

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n,i;
+printf("Enter the number");
+scanf("%d",&n);
+for(i=2;i<=n;i++)
+{
+	if(n%i==0)
+		break;
+}
+if(i==n)
+printf("\n%d is prime number",n);
+	else 
+		printf("\n%d is not prime number",n);
+
+}
