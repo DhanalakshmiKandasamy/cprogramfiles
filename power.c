d1cfec986ad45c7270308e893c71b9cd6fecbfba
DhanalakshmiKandasamy <dhanalakshmibca2@gmail.com>
2017-08-08 22:50:53 +0530

    Add files via upload

---
+++ power.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int n,s,i;
+printf("Enter the number");
+scanf("%d",&n);
+printf("Enter the power value");
+scanf("%d",&i);
+s=pow(n,i);
+printf("\n%d",s);
+}
